e182bc0e735cdd3631736b0914b0898b36e10de5
Thamindu97 <thamindusasitha8@gmail.com>
2021-10-01 20:16:47 +0530

    Big Decimal string conversion

--- big decimal/BigDecimalCode.java
+++ big decimal/BigDecimalCode.java
@@ -5,7 +5,7 @@ public class BigDecimalCode{
     	public static void main(String args[]){
 
 		// Initializing string array of 10 students marks
-       	 	Double[] marksArray = {70.5, 80.0, 54.35, 84.75, 91.0, 67.5, 86.0, 91.75, 88.5, 71.5};
+       	 	String[] marksArray = {"70.5", "80.0", "54.35", "84.75", "91.0", "67.5", "86.0", "91.75", "88.5", "71.5"};
 
         	// Number of students ( Size of the array )
         	int noOfStudents = marksArray.length; 	
@@ -18,7 +18,7 @@ public class BigDecimalCode{
         	// Calculating Average Marks
 		for(int i = 0; i < noOfStudents; i++)
         	{
-			marks = new BigDecimal(Double.toString(marksArray[i]));
+			marks = new BigDecimal(marksArray[i]);
             		totalMarks = totalMarks.add(marks); 
 		} 
   
